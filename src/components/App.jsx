12aa4ae1e418cb60941638e583b7229b7e331aa0
marlenawojtczak <wojtczakmarlena100@gmail.com>
2023-07-03 18:41:36 +0200

    added refectored code with hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { Container } from './App.styled';
-import {Searchbar} from './Searchbar/Searchbar';
+import { Searchbar } from './Searchbar/Searchbar';
 import ImageGallery from './ImageGallery/ImageGallery';
 import Button from './Button/Button';
 import Loader from './Loader/Loader';
@@ -9,54 +9,55 @@ import { fetchImages } from './Services/api';
 import Notiflix from 'notiflix';
 
 export const App = () => {
+  console.log('test');
 
-const [searchQuery, setSearchQuery] = useState('');
-const [page, setPage] = useState(1);
-const [images, setImages] = useState([]);
-const [selectedImage, setSelectedImage] = useState(null);
-const [isLoading, setIsLoading] = useState(false);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [page, setPage] = useState(1);
+  const [images, setImages] = useState([]);
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
-const fetchImagesWithQuery = async (query, page) => {
-  setIsLoading(true);
+  const fetchImagesWithQuery = async (query, page) => {
+    setIsLoading(true);
 
-  try {
-    const newImages = await fetchImages(query, page);
-    if (newImages.length === 0) {
-      Notiflix.Notify.info('No more images to load.');
+    try {
+      const newImages = await fetchImages(query, page);
+      if (newImages.length === 0) {
+        Notiflix.Notify.info('No more images to load.');
+        setIsLoading(false);
+      } else {
+        setImages(prevImages => [...prevImages, ...newImages]);
+        setIsLoading(false);
+        setPage(page);
+      }
+    } catch (error) {
+      console.log('Error fetching images:', error);
       setIsLoading(false);
-    } else {
-      setImages((prevImages) => [...prevImages, ...newImages]);
-      setIsLoading(false);
-      setPage(page);
     }
-  } catch (error) {
-    console.log('Error fetching images:', error);
-    setIsLoading(false);
-  }
-};
-
-const handleSubmit = (query) => {
-  setSearchQuery(query);
-  setPage(1);
-  setImages([]);
-  fetchImagesWithQuery(query, 1);
-};
-
- const loadMoreImages = () => {
-   const nextPage = page + 1;
-   fetchImagesWithQuery(searchQuery, nextPage);
   };
 
- const handleImageClick = image => {
-  setSelectedImage(image)
+  const handleSubmit = query => {
+    setSearchQuery(query);
+    setPage(1);
+    setImages([]);
+    fetchImagesWithQuery(query, 1);
   };
 
-  const  handleCloseModal = () => {
-    setSelectedImage(null)
+  const loadMoreImages = () => {
+    const nextPage = page + 1;
+    fetchImagesWithQuery(searchQuery, nextPage);
+  };
+
+  const handleImageClick = image => {
+    setSelectedImage(image);
+  };
+
+  const handleCloseModal = () => {
+    setSelectedImage(null);
   };
 
   useEffect(() => {
-    const handleKeyDown = (e) => {
+    const handleKeyDown = e => {
       if (e.keyCode === 27) {
         handleCloseModal();
       }
@@ -66,18 +67,17 @@ const handleSubmit = (query) => {
     return () => {
       document.removeEventListener('keydown', handleKeyDown);
     };
- }, [])
+  }, []);
 
-    return (
-      <Container>
-        <Searchbar onSubmit={handleSubmit} />
-        <ImageGallery images={images} onImageClick={handleImageClick} />
-        {isLoading && <Loader />}
-        {images.length > 0 && <Button onLoadMore={loadMoreImages} />}
-        {selectedImage && (
-          <Modal image={selectedImage} onClose={handleCloseModal} />
-        )}
-      </Container>
-    );
- 
-}
+  return (
+    <Container>
+      <Searchbar onSubmit={handleSubmit} />
+      <ImageGallery images={images} onImageClick={handleImageClick} />
+      {isLoading && <Loader />}
+      {images.length > 0 && <Button onLoadMore={loadMoreImages} />}
+      {selectedImage && (
+        <Modal image={selectedImage} onClose={handleCloseModal} />
+      )}
+    </Container>
+  );
+};
